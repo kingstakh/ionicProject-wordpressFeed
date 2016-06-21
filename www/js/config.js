497f10b80d18c1274d8125132874e441c15407c4
kingstakh <kingstakh@yandex.ru>
2016-06-21 19:40:43 +0300

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 angular.module('your_app_name.config', [])
-.constant('WORDPRESS_API_URL', 'https://wordpress.startapplabs.com/blog/api/')
+.constant('WORDPRESS_API_URL', 'https://kingstakh.by/api/')
 .constant('WORDPRESS_PUSH_URL', 'https://wordpress.startapplabs.com/blog/push/')
 .constant('GCM_SENDER_ID', '574597432927')
 
